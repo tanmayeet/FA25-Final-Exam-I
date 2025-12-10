8e6754bf67ed8c1ddf64bed070414d75d9506881
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-282.local>
2025-12-10 13:29:15 -0800

    Working on Milestone 1

--- main.cpp
+++ main.cpp
@@ -1,2 +1,17 @@
 // COMSC210 | Final 1 | Tanmayee Chalamalasetti
 // IDE Used: VS Code
+
+// Milestone 1
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+  map<string, int> traffic;
+  ifstream file("210-final-1-FA25-1.txt");
+  string origin, dest;
+
+  return 0;
+}
