dc467c2602d9f369d5548d126f55060983842b57
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-282.local>
2025-12-10 14:04:48 -0800

    Working on Milestone 3

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 #include <string>
 using namespace std;
 
+// Milestone 3
+void printInRange(const map<string, int> traffic, int low, int high) {
+  cout << "Airports within range [" << low << ", " << high << "]: \n";
+
+  for (const auto& entry : traffic) {
+    if (entry.second > low && entry.second < high) {
+      cout << entry.first << " " << entry.second << endl;
+    }
+  }
+}
+
 int main() {
   map<string, int> traffic;
   ifstream file("210-final-1-FA25-1.txt");
